c34b9b3dc1a67ffe3cc8a6d9a974aeb3348770ff
dmaslov <dmaslov@neoflex.ru>
2023-10-01 23:40:17 +0300

    create project

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <TheHeader />
-        <main className="container">{children}</main>
-        <TheFooter />
+        <div className="content">
+          <TheHeader />
+          <main className="container">{children}</main>
+          <TheFooter />
+        </div>
       </body>
     </html>
   );
